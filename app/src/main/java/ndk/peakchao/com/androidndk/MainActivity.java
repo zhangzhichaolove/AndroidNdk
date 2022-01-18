06549f8061ca92471e5673d4eb6220d85581ea21
peakchao <zhangzhichaolove@vip.qq.com>
2022-01-18 21:16:38 +0800

    update env.

--- app/src/main/java/ndk/peakchao/com/androidndk/MainActivity.java
+++ app/src/main/java/ndk/peakchao/com/androidndk/MainActivity.java
@@ -1,10 +1,11 @@
 package ndk.peakchao.com.androidndk;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
